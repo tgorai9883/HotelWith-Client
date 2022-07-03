2cef9c35576c7ce3a6eea8df48530962c84e3ba4
Tanmoy Gorai <tgorai9883@gmail.com>
2022-07-03 23:15:10 +0530

    added build version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import HomeScreen from './screens/HomeScreen';
+import BookingScreen from './screens/BookingScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import LoginScreen from './screens/LoginScreen.';
+import ProfileScreen from './screens/ProfileScreen';
+import AdminScreen from './screens/AdminScreen';
+import LandingScreen from './screens/LandingScreen';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/home" component={HomeScreen} exact/>
+          <Route path="/book/:roomid/:fromDate/:toDate" component={BookingScreen} exact/>
+          <Route path="/register" component={RegisterScreen} exact/>
+          <Route path="/login" component={LoginScreen} exact/>
+          <Route path="/profile" component={ProfileScreen} exact/>
+          <Route path="/admin" component={AdminScreen} exact/>
+          <Route path="/" component={LandingScreen} exact/>
+        </Switch>
+        
+      </BrowserRouter>
     </div>
   );
 }
